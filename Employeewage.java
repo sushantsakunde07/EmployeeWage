c562b287358ac8b572731b784307193cb5333d7b
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-07 06:56:22 +0530

    Added: Using switch case

--- Employeewage.java
+++ Employeewage.java
@@ -1,17 +1,23 @@
 package Employeewage;
 
 public class Employeewage {
+    public static final int	isFullTime=1;
+    public static final int partTime=2;
+    public static final int empRatePerHour=20;
     public static int employeeDailyWage() {
-            int	isFullTime=1;
-            int partTime=2;
-            int empRatePerHour=20;
             int employeehour=0;
             int employeeWage =0;
             int empcheck=(int)Math.floor(Math.random()* 10)% 3;
-            if (empcheck==isFullTime)
+        switch ((int)empcheck) {
+            case isFullTime:
                 employeehour=8;
-            else if (empcheck==partTime)
+                break;
+            case partTime:
                 employeehour=4;
+                break;
+            default:
+                employeehour=0;
+        }
             employeeWage = employeehour * empRatePerHour;
             System.out.println("empolyee wage:"   + employeeWage  );
 
